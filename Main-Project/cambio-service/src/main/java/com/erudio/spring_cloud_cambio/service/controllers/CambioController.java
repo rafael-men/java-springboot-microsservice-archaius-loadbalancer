e7c3d3fe31ca537328bdc5143df98a3fa99b9e98
rafael-men <rafaelgoncalves1822@gmail.com>
2024-09-19 19:58:37 -0300

    flyway error

--- Main-Project/cambio-service/src/main/java/com/erudio/spring_cloud_cambio/service/controllers/CambioController.java
+++ Main-Project/cambio-service/src/main/java/com/erudio/spring_cloud_cambio/service/controllers/CambioController.java
@@ -2,6 +2,7 @@ package com.erudio.spring_cloud_cambio.service.controllers;
 
 
 import com.erudio.spring_cloud_cambio.service.model.Cambio;
+import com.erudio.spring_cloud_cambio.service.repository.CambioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -18,11 +20,20 @@ public class CambioController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CambioRepository repository;
+
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount")BigDecimal amount,
                             @PathVariable("from")String from,
                             @PathVariable("to") String to){
         var port = environment.getProperty("local.server.port");
-        return new Cambio(1L, from, to, BigDecimal.ONE,BigDecimal.ONE,"PORT 8000");
+        var cambio = repository.findByFromAndTo(from, to);
+        if(cambio == null) throw new RuntimeException("Currency not Supported");
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnviroment(port);
+        return cambio;
     }
 }
--- Main-Project/cambio-service/src/main/java/com/erudio/spring_cloud_cambio/service/model/Cambio.java
+++ Main-Project/cambio-service/src/main/java/com/erudio/spring_cloud_cambio/service/model/Cambio.java
@@ -1,23 +1,33 @@
 package com.erudio.spring_cloud_cambio.service.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Entity(name = "cambio")
 public class Cambio implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "from_currency",nullable = false,length = 3)
     private String from;
 
+    @Column(name = "to_currency",nullable = false,length = 3)
     private String to;
 
+    @Column(nullable = false)
     private BigDecimal conversionFactor;
 
+    @Transient
     private BigDecimal convertedValue;
 
+    @Transient
     private String enviroment;
 
     public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String enviroment) {
