aea8c801ca420ab17f677d93ef036e1d089abd55
rafael-men <rafaelgoncalves1822@gmail.com>
2024-09-20 14:38:47 -0300

    fix-errors and book-service

---
+++ Main-Project/book-service/src/test/java/com/erudio/book_service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
