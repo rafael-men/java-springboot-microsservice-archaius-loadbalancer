05ece768d1f60d84759300ff233cdfee60517605
rafael-men <rafaelgoncalves1822@gmail.com>
2024-09-26 20:49:55 -0300

    eureka gateway

--- Main-Project/book-service/src/main/java/com/erudio/book_service/Controller/BookController.java
+++ Main-Project/book-service/src/main/java/com/erudio/book_service/Controller/BookController.java
@@ -39,7 +39,7 @@ public class BookController {
         }
        var cambio = proxy.getCambio(book.getPrice(),"USD",currency);
         var port = environment.getProperty("local.server.port");
-        book.setEnviroment(port + "FEIGN");
+        book.setEnviroment("Book Port: " + port + "Cambio Port" + cambio.getEnviroment());
         book.setPrice(cambio.getConvertedValue());
         return book;
     }
--- Main-Project/book-service/src/main/java/com/erudio/book_service/Proxy/CambioProxy.java
+++ Main-Project/book-service/src/main/java/com/erudio/book_service/Proxy/CambioProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(value = "cambio-service",url = "localhost:8000")
+@FeignClient(value = "cambio-service")
 public interface CambioProxy {
 
     @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
